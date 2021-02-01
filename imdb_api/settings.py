e1e7d4606153d693cc6f79fd4ce851261f079c87
kcdeepak16 <60536433+kcdeepak16@users.noreply.github.com>
2021-02-01 14:22:35 +0530

    wsgi changed

--- imdb_api/settings.py
+++ imdb_api/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'imdb_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [(os.path.abspath.join("templates"))],
+        'DIRS': [(os.path.join("templates"))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
