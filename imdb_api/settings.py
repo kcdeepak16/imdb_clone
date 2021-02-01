935748494bdf32949797452b65900cbde2b37f10
kcdeepak16 <60536433+kcdeepak16@users.noreply.github.com>
2021-02-01 13:45:54 +0530

    wsgi changed

--- imdb_api/settings.py
+++ imdb_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,4 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [(os.path.join("static"))]
+STATICFILES_DIRS = [(os.path.join(BASE_DIR,"static"))]
+STATIC_ROOT = (os.path.join(BASE_DIR, "static"))
+
