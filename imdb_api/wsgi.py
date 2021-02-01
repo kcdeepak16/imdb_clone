cfb19dbf619e1c19524b8859819a1e9dfa44cf3f
kcdeepak16 <60536433+kcdeepak16@users.noreply.github.com>
2021-02-01 14:09:08 +0530

    wsgi changed

--- imdb_api/wsgi.py
+++ imdb_api/wsgi.py
@@ -7,21 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-"""import os
+import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imdb_api.settings')
 
-application = get_wsgi_application()"""
-
-path = '/home/deepakkc007/imdb_api'
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'imdb_api.settings'
-
-
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
